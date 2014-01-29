6b145600859f1fece6c029d077059a382a819ead
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-29 12:57:35 +0100

    LICENSED to Indigo Development Team

--- src/Indigo/Queue/Job/AbstractJob.php
+++ src/Indigo/Queue/Job/AbstractJob.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Indigo Queue package.
  *
- * (c) IndigoPHP Development Team
+ * (c) Indigo Development Team
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
