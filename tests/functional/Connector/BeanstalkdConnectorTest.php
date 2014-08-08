0be2f225ee543c4333b3b21bac0de4865b9dfba0
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-08-08 15:06:28 +0200

    Adds header to tests

--- tests/functional/Connector/BeanstalkdConnectorTest.php
+++ tests/functional/Connector/BeanstalkdConnectorTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Indigo Queue package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Test\Functional;
 
 use Indigo\Queue\Connector\BeanstalkdConnector;
