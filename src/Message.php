daae73504f6c4569d5b0791569861e3f7a96a4c9
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-09-30 23:40:41 +0200

    Adds Message interface

---
+++ src/Message.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Indigo Queue package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Queue;
+
+/**
+ * Defines a generic interface for Messages
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+interface Message
+{
+    /**
+     * Returns the Message ID
+     *
+     * In case of responses it should return the job ID
+     *
+     * @return mixed
+     */
+    public function getId();
+
+    /**
+     * Returns the message data
+     *
+     * @return []
+     */
+    public function getData();
+
+    /**
+     * Sets the message data
+     *
+     * @param [] $data
+     *
+     * @return self
+     */
+    public function setData(array $data);
+
+    /**
+     * Returns the options
+     *
+     * @return []
+     */
+    public function getOptions();
+
+    /**
+     * Sets the options
+     *
+     * @param [] $options
+     *
+     * @return self
+     */
+    public function setOptions(array $options);
+}
